7971249819f7d68bfb65148a3f08107503bc8e33
FatemehGholinezhad <f.gholinezhad@gmail.com>
2022-03-31 14:48:38 +0200

    second commit :+1:

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,9 +20,9 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF /></a>
-        <a href="https://instagram.com"><FiInstagram /></a>
-        <a href="https://twitter.com"><IoLogoTwitter /></a>
+        <a href="https://facebook.com" target="_blank"><FaFacebookF /></a>
+        <a href="https://instagram.com" target="_blank"><FiInstagram /></a>
+        <a href="https://twitter.com" target="_blank"><IoLogoTwitter /></a>
       </div>
 
       <div className="footer__copyright">
